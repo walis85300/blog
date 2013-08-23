55409c6253b8272724de7ca4c9aabd14a65d48da
walis85300 <walojose46@gmail.com>
2013-08-22 22:39:05 -0430

    muestra en el index los tres últimos posts que se han publicado

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -3,13 +3,26 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from datetime import datetime
+from operator import itemgetter, attrgetter
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from publicaciones.models import *
 
 def index(request):
 
-	posts = Post.objects.all()
+	posts_lista = Post.objects.all()
+	paginator = Paginator(posts_lista,3)
+	pagina = request.GET.get('page')
+
+	try:
+		posts = paginator.page(pagina)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_page)
 	
-	return render_to_response('index.html',{'posts':posts},context_instance=RequestContext(request))
+	p = sorted(posts, key=attrgetter('id'),reverse=True)
+
+	return render_to_response('index.html',{'posts':p},context_instance=RequestContext(request))
 
 def post(request,id_post):
 	dato = get_object_or_404(Post,pk=id_post)
